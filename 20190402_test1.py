d64ff7bd0b84af9ae02d27e7f9e194826cdb32cf
NicoletteZhang <islandcoast@163.com>
2019-04-03 23:47:24 +0800

    1146

--- 20190402_test1.py
+++
@@ -1,9 +0,0 @@
-#机器学习库 numpy库
-#高性能科学计算和数据分析，是常用的高级数据分析库的基础包
-
-
-import numpy as np
-
-arr1 = np.array([2,3,4])
-print(arr1)
-print(arr1.dtype)
---
+++ 20190403_test1.py
@@ -0,0 +1,50 @@
+#机器学习库 numpy库
+#高性能科学计算和数据分析，是常用的高级数据分析库的基础包
+
+
+import numpy as np
+
+# arr1 = np.array([2,3,4])
+#
+# print(arr1)
+
+# print(arr1.dtype)
+#
+# arr2 = np.array([1.2,2.3,3.4])
+#
+# print(arr2)
+
+# print(arr2.dtype)
+#
+# print(arr1+arr2)
+#
+# print(arr2 * 10)
+#
+# data = [[1,2,3],[4,5,6]]
+
+# arr3 = np.array(data)
+
+# print(arr3)
+# print(arr3.dtype)
+
+# print(np.zeros((3,5)))
+#
+# print(np.ones((4,6)))
+#
+# print(np.empty((2,3,2)))
+
+# print(np.arange(10))
+
+#切片
+arr4 = np.arange(10)
+
+arr4[5:8] = 10
+
+print(arr4)
+
+arr_slice = arr4[5:8].copy()
+
+arr_slice[:] = 15
+
+print(arr_slice)
+print(arr4)

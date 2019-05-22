9c63086016142a139136b9db6e6341fa73b74d44
NicoletteZhang <islandcoast@163.com>
2019-05-22 22:58:16 +0800

    201905222258

--- test_code/decision_tree.py
+++ test_code/decision_tree.py
@@ -102,3 +102,97 @@ def chooseBestFeatureToSplit(dataSet):
 			bestInfoGain = infoGain 							#更新信息增益，找到最大的信息增益
 			bestFeature = i 									#记录信息增益最大的特征的索引值
 	return bestFeature 											#返回信息增益最大的特征的索引值
+
+
+# """
+# 函数说明:统计classList中出现此处最多的元素(类标签)
+# Parameters:
+# 	classList - 类标签列表
+# Returns:
+# 	sortedClassCount[0][0] - 出现此处最多的元素(类标签)
+# """
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:  # 统计classList中每个元素出现的次数
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)  # 根据字典的值降序排序
+    return sortedClassCount[0][0]  # 返回classList中出现次数最多的元素
+
+#
+# """
+# 函数说明:创建决策树
+# Parameters:
+# 	dataSet - 训练数据集
+# 	labels - 分类属性标签
+# 	featLabels - 存储选择的最优特征标签
+# Returns:
+# 	myTree - 决策树
+# """
+
+
+def createTree(dataSet, labels, featLabels):
+    classList = [example[-1] for example in dataSet]  # 取分类标签(是否放贷:yes or no)
+    if classList.count(classList[0]) == len(classList):  # 如果类别完全相同则停止继续划分
+        return classList[0]
+    if len(dataSet[0]) == 1 or len(labels) == 0:  # 遍历完所有特征时返回出现次数最多的类标签
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)  # 选择最优特征
+    bestFeatLabel = labels[bestFeat]  # 最优特征的标签
+    featLabels.append(bestFeatLabel)
+    myTree = {bestFeatLabel: {}}  # 根据最优特征的标签生成树
+    del (labels[bestFeat])  # 删除已经使用特征标签
+    featValues = [example[bestFeat] for example in dataSet]  # 得到训练集中所有最优特征的属性值
+    uniqueVals = set(featValues)  # 去掉重复的属性值
+    for value in uniqueVals:  # 遍历特征，创建决策树。
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels, featLabels)
+
+    return myTree
+
+
+# """
+# 函数说明:获取决策树叶子结点的数目
+# Parameters:
+# 	myTree - 决策树
+# Returns:
+# 	numLeafs - 决策树的叶子结点的数目
+# """
+
+
+def getNumLeafs(myTree):
+    numLeafs = 0  # 初始化叶子
+    firstStr = next(iter(
+        myTree))  # python3中myTree.keys()返回的是dict_keys,不在是list,所以不能使用myTree.keys()[0]的方法获取结点属性，可以使用list(myTree.keys())[0]
+    secondDict = myTree[firstStr]  # 获取下一组字典
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':  # 测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+#
+# """
+# 函数说明:获取决策树的层数
+# Parameters:
+# 	myTree - 决策树
+# Returns:
+# 	maxDepth - 决策树的层数
+# """
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0  # 初始化决策树深度
+    firstStr = next(iter(
+        myTree))  # python3中myTree.keys()返回的是dict_keys,不在是list,所以不能使用myTree.keys()[0]的方法获取结点属性，可以使用list(myTree.keys())[0]
+    secondDict = myTree[firstStr]  # 获取下一个字典
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':  # 测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth  # 更新层数
+    return maxDepth

dec72f16c63156f4bf1ad7cb7add029a18b566bd
NicoletteZhang <islandcoast@163.com>
2019-05-23 23:36:12 +0800

    201905232336

--- test_code/decision_tree.py
+++ test_code/decision_tree.py
@@ -196,3 +196,106 @@ def getTreeDepth(myTree):
             thisDepth = 1
         if thisDepth > maxDepth: maxDepth = thisDepth  # 更新层数
     return maxDepth
+
+# """
+# 函数说明:绘制结点
+# Parameters:
+# 	nodeTxt - 结点名
+# 	centerPt - 文本位置
+# 	parentPt - 标注的箭头位置
+# 	nodeType - 结点格式
+# Returns:
+# 	无
+# """
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+	arrow_args = dict(arrowstyle="<-")											#定义箭头格式
+	font = FontProperties(fname=r"c:\windows\fonts\simsunb.ttf", size=14)		#设置中文字体
+	createPlot.ax1.annotate(nodeTxt, xy=parentPt,  xycoords='axes fraction',	#绘制结点
+		xytext=centerPt, textcoords='axes fraction',
+		va="center", ha="center", bbox=nodeType, arrowprops=arrow_args, FontProperties=font)
+
+# """
+# 函数说明:标注有向边属性值
+# Parameters:
+# 	cntrPt、parentPt - 用于计算标注位置
+# 	txtString - 标注的内容
+# Returns:
+# 	无
+# """
+
+def plotMidText(cntrPt, parentPt, txtString):
+	xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]											#计算标注位置
+	yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
+	createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+
+# """
+# 函数说明:绘制决策树
+# Parameters:
+# 	myTree - 决策树(字典)
+# 	parentPt - 标注的内容
+# 	nodeTxt - 结点名
+# Returns:
+# 	无
+# """
+
+def plotTree(myTree, parentPt, nodeTxt):
+	decisionNode = dict(boxstyle="sawtooth", fc="0.8")										#设置结点格式
+	leafNode = dict(boxstyle="round4", fc="0.8")											#设置叶结点格式
+	numLeafs = getNumLeafs(myTree)  														#获取决策树叶结点数目，决定了树的宽度
+	depth = getTreeDepth(myTree)															#获取决策树层数
+	firstStr = next(iter(myTree))															#下个字典
+	cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yOff)	#中心位置
+	plotMidText(cntrPt, parentPt, nodeTxt)													#标注有向边属性值
+	plotNode(firstStr, cntrPt, parentPt, decisionNode)										#绘制结点
+	secondDict = myTree[firstStr]															#下一个字典，也就是继续绘制子结点
+	plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD										#y偏移
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__=='dict':											#测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+			plotTree(secondDict[key],cntrPt,str(key))        								#不是叶结点，递归调用继续绘制
+		else:																				#如果是叶结点，绘制叶结点，并标注有向边属性值
+			plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+			plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+			plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+	plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+
+# """
+# 函数说明:创建绘制面板
+# Parameters:
+# 	inTree - 决策树(字典)
+# Returns:
+# 	无
+# """
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')													#创建fig
+    fig.clf()																				#清空fig
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)    							#去掉x、y轴
+    plotTree.totalW = float(getNumLeafs(inTree))											#获取决策树叶结点数目
+    plotTree.totalD = float(getTreeDepth(inTree))											#获取决策树层数
+    plotTree.xOff = -0.5/plotTree.totalW; plotTree.yOff = 1.0;								#x偏移
+    plotTree(inTree, (0.5,1.0), '')															#绘制决策树
+    plt.show()																				#显示绘制结果
+
+# """
+# 函数说明:使用决策树分类
+# Parameters:
+# 	inputTree - 已经生成的决策树
+# 	featLabels - 存储选择的最优特征标签
+# 	testVec - 测试数据列表，顺序对应最优特征标签
+# Returns:
+# 	classLabel - 分类结果
+# """
+
+def classify(inputTree, featLabels, testVec):
+	firstStr = next(iter(inputTree))														#获取决策树结点
+	secondDict = inputTree[firstStr]														#下一个字典
+	featIndex = featLabels.index(firstStr)
+	for key in secondDict.keys():
+		if testVec[featIndex] == key:
+			if type(secondDict[key]).__name__ == 'dict':
+				classLabel = classify(secondDict[key], featLabels, testVec)
+			else: classLabel = secondDict[key]
+	return classLabel

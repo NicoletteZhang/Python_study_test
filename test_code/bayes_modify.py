6acd11fe49e9e09e881abd0504e801551e3ae2e9
NicoletteZhang <islandcoast@163.com>
2019-05-28 23:36:59 +0800

    201905282336

--- test_code/bayes_modify.py
+++ test_code/bayes_modify.py
@@ -75,3 +75,74 @@ def trainNB0(trainMatrix,trainCategory):
     p1Vect = np.log(p1Num/p1Denom)							#取对数，防止下溢出
     p0Vect = np.log(p0Num/p0Denom)
     return p0Vect,p1Vect,pAbusive							#返回属于侮辱类的条件概率数组，属于非侮辱类的条件概率数组，文档属于侮辱类的概率
+
+"""
+函数说明:朴素贝叶斯分类器分类函数
+Parameters:
+	vec2Classify - 待分类的词条数组
+	p0Vec - 非侮辱类的条件概率数组
+	p1Vec -侮辱类的条件概率数组
+	pClass1 - 文档属于侮辱类的概率
+Returns:
+	0 - 属于非侮辱类
+	1 - 属于侮辱类
+"""
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + np.log(pClass1)    	#对应元素相乘。logA * B = logA + logB，所以这里加上log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+"""
+函数说明:接收一个大字符串并将其解析为字符串列表
+Parameters:
+    无
+Returns:
+    无
+"""
+def textParse(bigString):                                                   #将字符串转换为字符列表
+    # * 会匹配0个或多个规则，split会将字符串分割成单个字符【python3.5+】; 这里使用\W 或者\W+ 都可以将字符数字串分割开，产生的空字符将会在后面的列表推导式中过滤掉
+    listOfTokens = re.split(r'\W+', bigString)                              #将特殊符号作为切分标志进行字符串切分，即非字母、非数字
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]            #除了单个字母，例如大写的I，其它单词变成小写
+
+"""
+函数说明:测试朴素贝叶斯分类器
+Parameters:
+    无
+Returns:
+    无
+"""
+def spamTest():
+    docList = []; classList = []; fullText = []
+    for i in range(1, 26):                                                  #遍历25个txt文件
+        wordList = textParse(open('email/spam/%d.txt' % i, 'r').read())     #读取每个垃圾邮件，并字符串转换成字符串列表
+        docList.append(wordList)
+        fullText.append(wordList)
+        classList.append(1)                                                 #标记垃圾邮件，1表示垃圾文件
+        wordList = textParse(open('email/ham/%d.txt' % i, 'r').read())      #读取每个非垃圾邮件，并字符串转换成字符串列表
+        docList.append(wordList)
+        fullText.append(wordList)
+        classList.append(0)                                                 #标记非垃圾邮件，1表示垃圾文件
+    vocabList = createVocabList(docList)                                    #创建词汇表，不重复
+    trainingSet = list(range(50)); testSet = []                             #创建存储训练集的索引值的列表和测试集的索引值的列表
+    for i in range(10):                                                     #从50个邮件中，随机挑选出40个作为训练集,10个做测试集
+        randIndex = int(random.uniform(0, len(trainingSet)))                #随机选取索索引值
+        testSet.append(trainingSet[randIndex])                              #添加测试集的索引值
+        del(trainingSet[randIndex])                                         #在训练集列表中删除添加到测试集的索引值
+    trainMat = []; trainClasses = []                                        #创建训练集矩阵和训练集类别标签系向量
+    for docIndex in trainingSet:                                            #遍历训练集
+        trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))       #将生成的词集模型添加到训练矩阵中
+        trainClasses.append(classList[docIndex])                            #将类别添加到训练集类别标签系向量中
+    p0V, p1V, pSpam = trainNB0(np.array(trainMat), np.array(trainClasses))  #训练朴素贝叶斯模型
+    errorCount = 0                                                          #错误分类计数
+    for docIndex in testSet:                                                #遍历测试集
+        wordVector = setOfWords2Vec(vocabList, docList[docIndex])           #测试集的词集模型
+        if classifyNB(np.array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:    #如果分类错误
+            errorCount += 1                                                 #错误计数加1
+            print("分类错误的测试集：",docList[docIndex])
+    print('错误率：%.2f%%' % (float(errorCount) / len(testSet) * 100))
+
+if __name__ == '__main__':
+    spamTest()

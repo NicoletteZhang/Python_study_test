1b393560eeba8e0c38c6f02630f0828655726552
NicoletteZhang <islandcoast@163.com>
2019-06-03 22:44:04 +0800

    201906032244

--- test_code/colicLogRegres.py
+++ test_code/colicLogRegres.py
@@ -96,3 +96,53 @@ def colicTest():
 			errorCount += 1
 	errorRate = (float(errorCount)/numTestVec) * 100 								#错误率计算
 	print("测试集错误率为: %.2f%%" % errorRate)
+
+
+"""
+函数说明:分类函数
+Parameters:
+	inX - 特征向量
+	weights - 回归系数
+Returns:
+	分类结果
+"""
+
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX*weights))
+    if prob > 0.5: return 1.0
+    else: return 0.0
+
+
+"""
+函数说明:使用Sklearn构建Logistic回归分类器
+Parameters:
+	无
+Returns:
+	无
+"""
+
+def colicSklearn():
+	frTrain = open('horseColicTraining.txt')										#打开训练集
+	frTest = open('horseColicTest.txt')												#打开测试集
+	trainingSet = []; trainingLabels = []
+	testSet = []; testLabels = []
+	for line in frTrain.readlines():
+		currLine = line.strip().split('\t')
+		lineArr = []
+		for i in range(len(currLine)-1):
+			lineArr.append(float(currLine[i]))
+		trainingSet.append(lineArr)
+		trainingLabels.append(float(currLine[-1]))
+	for line in frTest.readlines():
+		currLine = line.strip().split('\t')
+		lineArr =[]
+		for i in range(len(currLine)-1):
+			lineArr.append(float(currLine[i]))
+		testSet.append(lineArr)
+		testLabels.append(float(currLine[-1]))
+	classifier = LogisticRegression(solver = 'sag',max_iter = 5000).fit(trainingSet, trainingLabels)
+	test_accurcy = classifier.score(testSet, testLabels) * 100
+	print('正确率:%f%%' % test_accurcy)
+
+if __name__ == '__main__':
+	colicSklearn()
